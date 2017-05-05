942cf51e6540089396b562565faa6f4539769063
Aaron Heidgerken-Greene <aheidgerken@smm.org>
2017-05-05 16:02:16 -0500

    with resume

---
+++ data/desktop/About_Us/winActs.js
@@ -0,0 +1,60 @@
+
+var main = µ('win-dow[name=' + imports.window + ']')[0];
+var icon = µ('eye-con[name=' + imports.window + ']')[0];
+
+//console.log(main);
+var menu = main.menuBar;
+
+var fcont = µ('.frameContent', main.content)[0];
+var cont = µ('+div', fcont);
+var foot = main.foot;
+
+main.style.visibility = 'visible';
+
+var kids = icon.querySelectorAll('eye-con');
+for (var i = 0; i < kids.length; i++) {
+  cont.appendChild(kids[i]);
+  kids[i].style.display = 'inline-block';
+}
+
+µ('+div', foot).textContent = kids.length + ' file(s)';
+
+main.onClose = function(contn) {
+  var kids = contn.querySelectorAll('eye-con');
+  for (var i = 0; i < kids.length; i++) {
+    icon.appendChild(kids[i]);
+    kids[i].style.display = 'none';
+  }
+};
+
+var file = µ('+menu-item');
+file.addTitle('File');
+file.addOption('Open', function(argument) {
+  var sel = cont.selected;
+  if (sel) sel.openWindow();
+});
+
+file.addOption('Open with...');
+file.addOption('Print...');
+
+file.addDivider();
+file.addOption('Delete');
+
+file.addDivider();
+file.addOption('Close Window', function() {
+  main.closeWindow();
+});
+
+menu.appendChild(file);
+
+var edit = document.createElement('menu-item');
+edit.addTitle('Edit');
+edit.addOption('Undo');
+edit.addOption('Redo');
+edit.addDivider();
+edit.addOption('Copy');
+edit.addOption('Paste');
+
+menu.appendChild(edit);
+
+main.resetContentHeight();
---
+++ data/desktop/Resume/winActs.js
@@ -0,0 +1,39 @@
+var main = µ('win-dow[name=' + imports.window + ']')[0];
+console.log(imports.window);
+var menu = main.menuBar;
+
+/*var file = µ('+menu-item');
+file.addTitle('File');
+file.addOption('Open');
+
+file.addOption('Open folder...');
+file.addDivider();
+file.addOption('Print...');
+
+file.addDivider();
+file.addOption('Close', function() {
+  main.closeWindow();
+});
+
+menu.appendChild(file);
+
+var edit = document.createElement('menu-item');
+edit.addTitle('Edit');
+edit.addOption('Undo');
+edit.addOption('Redo');
+edit.addDivider();
+edit.addOption('Copy');
+edit.addOption('Paste');
+
+menu.appendChild(edit);
+
+var help = document.createElement('menu-item');
+help.addTitle('Help');
+help.addOption('Help...');
+
+menu.appendChild(help);*/
+
+main.changeSize(640, 640);
+
+main.resetContentHeight();
+main.style.visibility = 'visible';
