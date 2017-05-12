fb0915f2ad820856e01495f59433394be450026c
Aaron Heidgerken-Greene <aheidgerken@smm.org>
2017-05-12 14:32:18 -0500

    potential fix for menu-item error

--- data/desktop/About_Us/winActs.js
+++ data/desktop/About_Us/winActs.js
@@ -1,60 +1,63 @@
+obtain([window.retroDir + 'menuItems.js'], (MI)=> {
+  var main = µ('win-dow[name=' + imports.window + ']')[0];
+  var icon = µ('eye-con[name=' + imports.window + ']')[0];
 
-var main = µ('win-dow[name=' + imports.window + ']')[0];
-var icon = µ('eye-con[name=' + imports.window + ']')[0];
+  //console.log(main);
+  var menu = main.menuBar;
 
-//console.log(main);
-var menu = main.menuBar;
+  var fcont = µ('.frameContent', main.content)[0];
+  var cont = µ('+div', fcont);
+  var foot = main.foot;
 
-var fcont = µ('.frameContent', main.content)[0];
-var cont = µ('+div', fcont);
-var foot = main.foot;
-
-var kids = icon.querySelectorAll('eye-con');
-for (var i = 0; i < kids.length; i++) {
-  cont.appendChild(kids[i]);
-  kids[i].style.display = 'inline-block';
-}
-
-µ('+div', foot).textContent = kids.length + ' file(s)';
-
-main.onClose = function(contn) {
-  var kids = contn.querySelectorAll('eye-con');
+  var kids = icon.querySelectorAll('eye-con');
   for (var i = 0; i < kids.length; i++) {
-    icon.appendChild(kids[i]);
-    kids[i].style.display = 'none';
+    cont.appendChild(kids[i]);
+    kids[i].style.display = 'inline-block';
   }
-};
 
-var file = new customElements.get('menu-item')();
-console.log(file);
-file.addTitle('File');
-file.addOption('Open', function(argument) {
-  var sel = cont.selected;
-  if (sel) sel.openWindow();
+  µ('+div', foot).textContent = kids.length + ' file(s)';
+
+  main.onClose = function(contn) {
+    var kids = contn.querySelectorAll('eye-con');
+    for (var i = 0; i < kids.length; i++) {
+      icon.appendChild(kids[i]);
+      kids[i].style.display = 'none';
+    }
+  };
+
+  var file = new MI.menuItem();
+  console.log(file);
+  file.addTitle('File');
+  file.addOption('Open', function(argument) {
+    var sel = cont.selected;
+    if (sel) sel.openWindow();
+  });
+
+  file.addOption('Open with...');
+  file.addOption('Print...');
+
+  file.addDivider();
+  file.addOption('Delete');
+
+  file.addDivider();
+  file.addOption('Close Window', function() {
+    main.closeWindow();
+  });
+
+  menu.appendChild(file);
+
+  var edit = document.createElement('menu-item');
+  edit.addTitle('Edit');
+  edit.addOption('Undo');
+  edit.addOption('Redo');
+  edit.addDivider();
+  edit.addOption('Copy');
+  edit.addOption('Paste');
+
+  menu.appendChild(edit);
+
+  main.resetContentHeight();
+  main.style.visibility = 'visible';
+
+  provide(exports);
 });
-
-file.addOption('Open with...');
-file.addOption('Print...');
-
-file.addDivider();
-file.addOption('Delete');
-
-file.addDivider();
-file.addOption('Close Window', function() {
-  main.closeWindow();
-});
-
-menu.appendChild(file);
-
-var edit = document.createElement('menu-item');
-edit.addTitle('Edit');
-edit.addOption('Undo');
-edit.addOption('Redo');
-edit.addDivider();
-edit.addOption('Copy');
-edit.addOption('Paste');
-
-menu.appendChild(edit);
-
-main.resetContentHeight();
-main.style.visibility = 'visible';
